7de328a23d3aaf0b3a968f0dac9bf4afd6b8b846
miszta COH <misztacoh@hotmail.com>
2020-10-04 22:10:45 +0200

    Kettő

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,9 +12,33 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.User> users = new BindingList<Entities.User>();
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource.LastName; // label1
+            label2.Text = Resource.FirstName; // label2
+            button1.Text = Resource.Add; // button1
+
+            // listbox1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new Entities.User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
