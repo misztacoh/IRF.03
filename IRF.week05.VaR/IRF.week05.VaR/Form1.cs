d822b370b22f60de37ff763d225530600186254b
miszta COH <misztacoh@hotmail.com>
2020-10-18 17:51:59 +0200

    #6

--- IRF.week05.VaR/IRF.week05.VaR/Form1.cs
+++ IRF.week05.VaR/IRF.week05.VaR/Form1.cs
@@ -33,6 +33,21 @@ namespace IRF.week05.VaR
             dataGridView2.DataSource = Portfolio;
         }
 
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
