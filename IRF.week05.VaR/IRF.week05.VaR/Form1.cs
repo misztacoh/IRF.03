8785f32d24a70dd210437bd15a000452bec231b7
miszta COH <misztacoh@hotmail.com>
2020-10-18 17:42:40 +0200

    #4

--- IRF.week05.VaR/IRF.week05.VaR/Form1.cs
+++ IRF.week05.VaR/IRF.week05.VaR/Form1.cs
@@ -12,9 +12,18 @@ namespace IRF.week05.VaR
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
