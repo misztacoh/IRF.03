7304e3f6baa6ebed49487014a9e3a9fa919f5852
miszta COH <misztacoh@hotmail.com>
2020-11-08 21:11:19 +0100

    #6

--- IRF.WEEk07.Simulation/IRF.WEEk07.Simulation/Form1.cs
+++ IRF.WEEk07.Simulation/IRF.WEEk07.Simulation/Form1.cs
@@ -17,6 +17,7 @@ namespace IRF.WEEk07.Simulation
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +25,25 @@ namespace IRF.WEEk07.Simulation
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         private List<DeathProbability> GetDeathProbabilities(string csvpath)
