83bb1404236094f9121335506dde852e915717d0
miszta COH <misztacoh@hotmail.com>
2020-12-04 14:38:13 +0100

    #2-3

--- IRF.WEEK.10.WorldHardestGame/IRF.WEEK.10.WorldHardestGame/Form1.cs
+++ IRF.WEEK.10.WorldHardestGame/IRF.WEEK.10.WorldHardestGame/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace IRF.WEEK._10.WorldHardestGame
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
