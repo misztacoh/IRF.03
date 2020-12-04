d651e18559c8d366a99027b7676cd2c8f2325f68
miszta COH <misztacoh@hotmail.com>
2020-12-04 14:40:37 +0100

    #4

--- IRF.WEEK.10.WorldHardestGame/IRF.WEEK.10.WorldHardestGame/Form1.cs
+++ IRF.WEEK.10.WorldHardestGame/IRF.WEEK.10.WorldHardestGame/Form1.cs
@@ -16,6 +16,11 @@ namespace IRF.WEEK._10.WorldHardestGame
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,8 +28,15 @@ namespace IRF.WEEK._10.WorldHardestGame
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            //gc.AddPlayer();
+            //gc.Start(true);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
