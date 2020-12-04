f2d578c72e234d21c52186e95e8a64b0128ddbd8
miszta COH <misztacoh@hotmail.com>
2020-12-04 14:46:40 +0100

    #6

--- IRF.WEEK.10.WorldHardestGame/IRF.WEEK.10.WorldHardestGame/Form1.cs
+++ IRF.WEEK.10.WorldHardestGame/IRF.WEEK.10.WorldHardestGame/Form1.cs
@@ -47,6 +47,29 @@ namespace IRF.WEEK._10.WorldHardestGame
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+            gc.ResetCurrentLevel();
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+
+            gc.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
